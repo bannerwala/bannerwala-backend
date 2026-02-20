6d48d9abc4eee39f1111fa430f4f8c929a8eb517
ishwari.kanase <codadhyay@gmail.com>
2026-02-20 15:23:25 +0530

    Image size

--- modules/Users/user.helper.mjs
+++ modules/Users/user.helper.mjs
@@ -4,7 +4,7 @@ const storage = multer.memoryStorage();
 
 export const uploadUserImages = multer({
   storage,
-  limits: { fileSize: 10 * 1024 * 1024 } // 10MB limit
+  limits: { fileSize: 100 * 1024 * 1024 } // 10MB limit
 }).fields([
   { name: 'profile_pic', maxCount: 1 },
   { name: 'background_removed_pic', maxCount: 1 }
