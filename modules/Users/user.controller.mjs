73e8a1f0b47032a2d4904eed2e7bd74dd98ef3d2
ishwari.kanase <codadhyay@gmail.com>
2026-02-24 12:19:06 +0530

    Bannerwala otp changes

--- modules/Users/user.controller.mjs
+++ modules/Users/user.controller.mjs
@@ -109,7 +109,7 @@ export const sendOtp = async (req, res) => {
     await client.messages.create({
       to: `+91${contact_number}`,
       from: '+14127753820',
-      body: `OTP for 3_Extent is ${otp}`
+      body: `OTP for Bannerwala is ${otp}`
     });
 
     res.json({
